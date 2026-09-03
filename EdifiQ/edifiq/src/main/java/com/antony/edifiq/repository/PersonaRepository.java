ba7623b48826d804e37463240f0ce2ac8ec89ac7
antony1804 <antonymercadobet56@gmail.com>
2026-09-02 19:59:29 -0500

    delete proyects

--- EdifiQ/edifiq/src/main/java/com/antony/edifiq/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.antony.edifiq.repository;
-
-import com.antony.edifiq.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
--- EdifiQ/edifiq/src/main/java/com/antony/edifiq/repository/TipoDocumentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.antony.edifiq.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.antony.edifiq.model.TipoDocumento;
-
-public interface TipoDocumentoRepository extends JpaRepository<TipoDocumento, Long> {
-}
